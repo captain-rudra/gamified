1fa4a6ef052e9946079823dd157aec0d60455423
captain-rudra <rudrabanna45@gmail.com>
2025-12-07 01:34:00 +0530

    Update API base URL to production server to backend

--- Frontend/src/api/quizApi.js
+++ Frontend/src/api/quizApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend base URL
+  baseURL: "https://gamified-1.onrender.com/api", // your backend base URL
 });
+
